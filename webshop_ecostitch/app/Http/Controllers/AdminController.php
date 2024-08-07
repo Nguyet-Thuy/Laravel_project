f3a393180202fa0f6d4fcd4f8bad1929c01ff494
Nguyet-Thuy <133093684+Nguyet-Thuy@users.noreply.github.com>
2024-08-07 23:36:18 +0200

    adminpanel adjustments

---
+++ webshop_ecostitch/app/Http/Controllers/AdminController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Category;
+
+class AdminController extends Controller
+{
+    public function view_category()
+    {
+        $data = Category::all();
+        return view('admin.category',compact('data'));
+    }
+
+    public function add_category(Request $request)
+    {
+        $category = new Category;
+
+        $category->category_name = $request->category;
+
+        $category->save();
+
+        toastr()->timeOut(10000)->closeButton()->addSuccess('Category Added Succesfully');
+
+        return redirect()->back();
+    }
+
+    public function delete_category($id)
+    {
+        $data = Category::find($id);
+
+        $data->delete();
+
+        toastr()->timeOut(10000)->closeButton()->addSuccess('Category Deleted Succesfully');
+
+        return redirect()->back();
+    }
+}
